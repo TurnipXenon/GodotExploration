ca4728ce574fd0e8241194349160c133babaaea2
TurnipXenon <turnipxenon@gmail.com>
2022-10-02 17:13:03 -0600

    Make ball move (#8)
    * Initial project
    
    * Make ball move and bounce
    
    * Add start velocity randomization
    
    * Add explicity types
    
    * Add roadmap place in readme

--- scenes/simple_pong/components/paddle/paddle_controller.gd
+++ scenes/simple_pong/components/paddle/paddle_controller.gd
@@ -1,21 +1,22 @@
 extends KinematicBody2D
 
-const InputEnum = preload("res://common/scripts/input_enum.gd")
+const InputEnum: Resource = preload("res://common/scripts/input_enum.gd")
 
-export var input_strength: float  = 4
-export var control_scheme: int = InputEnum.ControlScheme.PLAYER
+export(float) var input_strength = 4.0
+export(InputEnum.ControlScheme) var control_scheme = InputEnum.ControlScheme.PLAYER
 
-var up_key = "ui_up"
-var down_key = "ui_down"
+var up_key: String = "ui_up"
+var down_key: String = "ui_down"
 
 func _ready():
-   var prefix = InputUtil.get_prefix(control_scheme)
+   var prefix: String = InputUtil.get_prefix(control_scheme)
    up_key = "%s_up" % prefix
    down_key = "%s_down" % prefix
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-   var input_value = Vector2.ZERO
+   var input_value: Vector2 = Vector2.ZERO
    input_value.y = Input.get_action_strength(down_key) - Input.get_action_strength(up_key)
+   # warning-ignore:return_value_discarded
    move_and_collide(input_value * delta * input_strength)
 
