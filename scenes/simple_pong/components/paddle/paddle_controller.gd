c6285c95968b4ab0749a5e05a20e70f5998464ff
TurnipXenon <turnipxenon@gmail.com>
2022-10-08 17:49:24 -0600

    Refactor to prevent hardcoded preload for enum

--- scenes/simple_pong/components/paddle/paddle_controller.gd
+++ scenes/simple_pong/components/paddle/paddle_controller.gd
@@ -8,14 +8,14 @@ var down_key: String = "ui_down"
 
 
 func _ready():
-	var prefix: String = InputUtil.get_prefix(control_scheme)
-	up_key = "%s_up" % prefix
-	down_key = "%s_down" % prefix
+    var prefix: String = InputUtil.get_prefix(control_scheme)
+    up_key = "%s_up" % prefix
+    down_key = "%s_down" % prefix
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var input_value: Vector2 = Vector2.ZERO
-	input_value.y = Input.get_action_strength(down_key) - Input.get_action_strength(up_key)
-	# warning-ignore:return_value_discarded
-	move_and_collide(input_value * delta * input_strength)
+    var input_value: Vector2 = Vector2.ZERO
+    input_value.y = Input.get_action_strength(down_key) - Input.get_action_strength(up_key)
+    # warning-ignore:return_value_discarded
+    move_and_collide(input_value * delta * input_strength)
