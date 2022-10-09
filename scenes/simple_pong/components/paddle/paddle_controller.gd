08c8f2c120a379a4e4d11fc88d0ccc60cf760b81
TurnipXenon <turnipxenon@gmail.com>
2022-10-08 23:57:24 -0600

    Move paddle keyboard controller logic out of the paddle controller

--- scenes/simple_pong/components/paddle/paddle_controller.gd
+++ scenes/simple_pong/components/paddle/paddle_controller.gd
@@ -1,21 +1,22 @@
+class_name PaddleController
 extends KinematicBody2D
 
 export(float) var input_strength = 4.0
 export(GameEnums.ControlScheme) var control_scheme = GameEnums.ControlScheme.PLAYER
+export(Script) var behavior_script
 
-var up_key: String = "ui_up"
-var down_key: String = "ui_down"
+var behavior = null
 
 
 func _ready():
-	var prefix: String = InputUtil.get_prefix(control_scheme)
-	up_key = "%s_up" % prefix
-	down_key = "%s_down" % prefix
+	assert(behavior_script != null)
+	behavior = behavior_script.new()
+	assert(behavior.has_method("initialize"))
+	assert(behavior.has_method("act"))
+	behavior.initialize(self)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var input_value: Vector2 = Vector2.ZERO
-	input_value.y = Input.get_action_strength(down_key) - Input.get_action_strength(up_key)
 	# warning-ignore:return_value_discarded
-	move_and_collide(input_value * delta * input_strength)
+	move_and_collide(behavior.act(self, delta))
