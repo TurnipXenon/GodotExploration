dc99ffffbe9e97ee3ab50093473b6ad0e1f99985
TurnipXenon <turnipxenon@gmail.com>
2022-10-15 21:50:09 -0600

    Upgrade gdtoolkit (#52)

--- common/utils/debug/debug_config.gd
+++ common/utils/debug/debug_config.gd
@@ -1,4 +1,4 @@
 class_name DebugConfig
 extends Resource
 
-@export var log_level = GameConstants.LogLevel.LASSERT # (GameConstants.LogLevel)
+@export var log_level = GameConstants.LogLevel.LASSERT  # (GameConstants.LogLevel)
