b342aaf28e7a08db44839e833cc257e6099fac70
turnipxenon <turnipxenon@gmail.com>
2022-10-29 01:11:00 -0600

    Add BreakoutMenu

--- Scripts/Common/ResourcePaths.cs
+++ Scripts/Common/ResourcePaths.cs
@@ -3,6 +3,23 @@
 public static class ResourcePaths
 {
 	private const string BasePath = "res://Scripts/Scenes";
+
+	public static class Breakout
+	{
+		private const string BreakoutBasePath = $"{BasePath}/Breakout";
+		public static class BreakoutMenu
+		{
+			private const string BreakoutMenuBasePath = $"{BreakoutBasePath}/BreakoutMenu";
+			public const string BreakoutMenuScene = $"{BreakoutMenuBasePath}/BreakoutMenu.tscn";
+		}
+	}
+
+	public static class MainMenu
+	{
+		private const string MainMenuBasePath = $"{BasePath}/MainMenu";
+		public const string MainMenuScene = $"{MainMenuBasePath}/MainMenu.tscn";
+	}
+	
 	public static class SimplePong
 	{
 		private const string SimplePongBasePath = $"{BasePath}/simple_pong";
---
+++ Scripts/Scenes/Breakout/BreakoutMenu/BreakoutMenu.cs
@@ -0,0 +1,13 @@
+﻿using Godot;
+using GodotExploration.Scripts.Common;
+using GodotExploration.Scripts.Common.Utils;
+
+namespace GodotExploration.Scripts.Scenes.Breakout.BreakoutMenu;
+
+public partial class BreakoutMenu: Node
+{
+	private void OnBackButtonPressed()
+	{
+		SceneUtil.GetSingleton(this).GoToScene(ResourcePaths.MainMenu.MainMenuScene, null);
+	}
+}
