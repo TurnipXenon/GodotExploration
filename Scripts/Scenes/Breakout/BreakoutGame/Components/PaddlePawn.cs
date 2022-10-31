02bc6a93a2a9cd712bd0dd32c0caf3ccda7032d4
turnipxenon <turnipxenon@gmail.com>
2022-10-30 20:14:24 -0600

    WIP

--- Scripts/Scenes/Breakout/BreakoutGame/Components/PaddlePawn.cs
+++ Scripts/Scenes/Breakout/BreakoutGame/Components/PaddlePawn.cs
@@ -2,15 +2,16 @@ using Godot;
 
 namespace GodotExploration.Scripts.Scenes.Breakout.BreakoutGame.Components;
 
-public partial class PaddlePawn : Node
+public partial class PaddlePawn : CharacterBody2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    [Export]
+    public float InputStrength = 4.0f;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+
+    public override void _Process(double delta)
+    {
+        var inputValue = Vector2.Zero;
+        inputValue.x = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
+        MoveAndCollide(inputValue * (float)delta * InputStrength);
+    }
 }
