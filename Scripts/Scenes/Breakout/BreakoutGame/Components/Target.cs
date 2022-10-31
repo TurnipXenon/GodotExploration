24321fd4e3d4109ccff9dd6175becfd247a13715
turnipxenon <turnipxenon@gmail.com>
2022-10-31 01:38:46 -0600

    Generate targets via code

---
+++ Scripts/Scenes/Breakout/BreakoutGame/Components/Target.cs
@@ -0,0 +1,9 @@
+﻿using Godot;
+
+namespace GodotExploration.Scripts.Scenes.Breakout.BreakoutGame.Components;
+
+public partial class Target : StaticBody2D
+{
+    // todo(turnip): generate color depending on lives
+    // todo(turnip): lives
+}
